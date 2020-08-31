c42b08878bc41e5d347e2f70ef6d14cd7d648511
KJ <29124655+deutranium@users.noreply.github.com>
2020-08-31 05:31:44 +0530

    Sample staging

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+	return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
