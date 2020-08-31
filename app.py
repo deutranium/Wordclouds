0f85ca0f5c0f23de029019ccb100c05d6ab70a6a
KJ <29124655+deutranium@users.noreply.github.com>
2020-09-01 00:16:47 +0530

    Added basic template

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import pandas as pd
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-	return "Hello World!"
+def index():
+	# return "Hello World!"
+	return render_template('index.html')
 
 
-@app.route('/<name>')
-def hello_name(name):
-	return "Hello {}!".format(name)
+# @app.route('/<name>')
+# def hello_name(name):
+# 	return "Hello {}!".format(name)
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		contents = pd.read_csv(secure_filename(f.filename))
+		print(contents)
+		return "done"
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
