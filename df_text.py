83fdc1643b954fdc5dc7cd4d4549abb06449da49
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2020-09-02 18:16:33 +0530

    take chats from DataFrame and join them into to 1 large string

--- df_text.py
+++ df_text.py
@@ -1,3 +1,14 @@
+def clean_text(in_string):
+	in_string = in_string.split(" ")
+	while True:
+		try:
+			in_string.remove("")
+		except:
+			break
+	return " ".join(in_string)
+
 def df_to_text(df):
-	text = ""
-	print(text)
+	chats = df.Message
+	chats = [str(chat) for chat in chats]
+	text = " ".join(chats)
+	return clean_text(text)
