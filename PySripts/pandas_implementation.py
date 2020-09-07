0bb5ced12efa54d4de7c96e790bc2a43c78e4ad7
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2020-09-07 13:46:16 +0530

    Fixed Date error, added working Date range inputs

--- PySripts/pandas_implementation.py
+++ PySripts/pandas_implementation.py
@@ -5,12 +5,31 @@ import datetime
 
 def deEmojify(txt):
 	converted=""
-
 	for each in txt:
 		if each not in emoji.UNICODE_EMOJI:
 			converted = converted + each.lower()
 	return converted
 
+def remove_unwanted_texts(txt):
+	"""
+		Removed messeges are of the type:
+			'<media omitted>': Used by whatsapp to tell where a file was uploaded
+			"(file attached)": Also Used by whatsapp to tell where a file was uploaded
+			"this message was deleted": Used by whatsapp to tell where other users deleted a message
+			"you message was deleted":  Used by whatsapp to tell where user deleted a message
+		"""
+	if (txt != '<media omitted>') & (txt.find("(file attached)")==-1) & (txt !="this message was deleted") & (txt!="you deleted this message"):
+		return 1
+	else:
+		return 0
+
+def correct_dateformat(data):
+	for i in range(len(data.Date.iloc[:])):
+		if (len(data.Date.iloc[i])==10):
+			data.Date.iloc[i] = pd.to_datetime(data.Date.iloc[i], format="%d/%m/%Y")
+		else:
+			data.Date.iloc[i] = pd.to_datetime(data.Date.iloc[i], format="%m/%d/%y")
+	return data
 
 # remove new line characters within individual messages
 def convert_chat_arr(chat):
@@ -33,7 +52,6 @@ def convert_chat_arr(chat):
 	for i in range(len(chat)):
 		if chat[i].split(' ')[0] == 'NA':
 			chat[i] = 'NA'
-
 	while True:
 		try:
 			chat.remove("NA")
@@ -44,16 +62,14 @@ def convert_chat_arr(chat):
 
 	for i in chat:
 		try:
-			chat_arr = []
-
-			# generates ['', 'date', ', ', 'time', ' - ', 'sender', ': ', 'message', '']
 			matches = re.split('^(.*?)(, | - |: )(.*?)(?!\2)(, | - |: )(.*?)(?!\2|\4)(, | - |: )(.*)', i, re.MULTILINE)
-
-			chat_arr.append(matches[1])	# date
-			chat_arr.append(matches[3]) # time
-			chat_arr.append(matches[5]) # sender
-			chat_arr.append(deEmojify(matches[7])) # message
-			final.append(chat_arr)
+			if remove_unwanted_texts(deEmojify(matches[7])):
+				chat_arr = []
+				chat_arr.append(matches[1]) # date
+				chat_arr.append(matches[3]) # time
+				chat_arr.append(matches[5]) # sender
+				chat_arr.append(deEmojify(matches[7])) # message
+				final.append(chat_arr)
 		except:
 			continue
 
@@ -61,8 +77,6 @@ def convert_chat_arr(chat):
 
 
 def get_DataFrame(input):
-
 	chat_arr = convert_chat_arr(input)
 	chats = pd.DataFrame(chat_arr, columns=["Date", "Time", "Sender", "Message"])
-
-	return chats
+	return correct_dateformat(chats)
--- PySripts/wordcloud_generator.py
+++ PySripts/wordcloud_generator.py
@@ -10,20 +10,5 @@ stopwords = set(STOPWORDS)
 
 def create_wc(text):
 	cloud = WordCloud(background_color = "white", max_words = 200, stopwords = set(STOPWORDS)).generate(text)
-	
-	# plt.figure(1,figsize=(13, 13))
-	# plt.imshow(cloud)
-	# plt.axis('off')
-	# plt.title('Wordcloud of key')
-	# plt.savefig('./static/WordCloud.png')
-	# plt.close()
-	# print(text)
-
-
 	cloud.to_file("./static/WordCloud.png")
-
 	return 1
-
-
-# a = create_wc("ab ab ab ab ab ab ab ab cd cd ef ef ef ef ef ab ab ab ab ab ab ef gh ij kl mn op qr st uv wx wx wx yz")
-# a = a.save("abcdefg")
