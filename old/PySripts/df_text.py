f4d2ef28eb8741f2ef9759458727366b4ee7f088
KJ <29124655+deutranium@users.noreply.github.com>
2020-09-08 04:30:49 +0530

    removed old files

--- old/PySripts/df_text.py
+++
@@ -1,5 +0,0 @@
-def df_to_text(df):
-	chats = df.Message
-	chats = [str(chat) for chat in chats]
-	text = " ".join(chats)
-	return text
--- old/PySripts/wordcloud_generator.py
+++
@@ -1,7 +0,0 @@
-from wordcloud import WordCloud, STOPWORDS
-stopwords = set(STOPWORDS)
-
-def create_wc(text):
-	cloud = WordCloud(background_color = "white", max_words = 200, stopwords = set(STOPWORDS)).generate(text)
-	cloud.to_file("./static/WordCloud.png")
-	return 1
