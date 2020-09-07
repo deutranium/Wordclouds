04571381e00f2343377ea8abbf60f04969137fde
KJ <29124655+deutranium@users.noreply.github.com>
2020-09-08 03:56:42 +0530

    Check #5

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ def index():
 
 @app.route('/', methods = ['POST'])
 def temp_code():
+	f = request.files['file']
+	decoded = f.read().decode('utf-8')
+	Data_extracted = get_DataFrame(decoded)
+	start_date = request.form.get("start_date")
+	end_date = request.form.get("end_date")
+
+	txt = df_to_text(Data_extracted)
+	wc_created = create_wc(txt)
+
 	print("posted")
-	return 1
+	return render_template('index.html', plot=1, url ='/static/WordCloud.png')
 
 if __name__ == '__main__':
 		app.run()
